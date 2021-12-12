748b4094c9574394750e1111a9712b588cd7b8b8
Alexander Radchenko <alex.radchenko.by@gmail.com>
2021-12-12 22:48:02 +0300

    1: Do general tasks 2

--- 1/src/com/company/test/Test.java
+++ 1/src/com/company/test/Test.java
@@ -1,8 +1,8 @@
 package com.company.test;
 
 public class Test {
-    public static void main(String[] args)  {
-      doTask3();
+    public static void main(String[] args) {
+        doTask4();
     }
 
     public static void doTask1() {
@@ -31,9 +31,22 @@ public class Test {
     public static void doTask3() {
         int T = 124_123;
         int hours = T / 3600;
-        int remainderSeconds =  T - hours * 3600;
+        int remainderSeconds = T - hours * 3600;
         int minutes = remainderSeconds / 60;
         int seconds = remainderSeconds % 60;
         System.out.println(hours + "ч " + minutes + "мин " + seconds + "с");
     }
-}
+
+    public static void doTask4() {
+        int angle1 = 45;
+        int angle2 = 100;
+        int angle3 = 180 - (angle1 + angle2);
+        if (angle1 + angle2 < 180 && angle1 == 90 || angle2 == 90 || angle3 == 90) {
+            System.out.println("Треугольник существует " + "Прямой треугольник");
+        } else {
+            System.out.println("Треугольник существует " + "Не прямой треугольник");
+            }
+        }
+    }
+
+
