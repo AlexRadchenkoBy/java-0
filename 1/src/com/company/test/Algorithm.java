c9dda587443af12a73be0c8d21ab71c475af89af
Alexander Radchenko <alex.radchenko.by@gmail.com>
2021-12-26 21:08:50 +0300

    Algorithm 2

--- 1/src/com/company/test/Algorithm.java
+++ 1/src/com/company/test/Algorithm.java
@@ -3,6 +3,9 @@ package com.company.test;
 public class Algorithm {
 
     public static void main(String[] args) {
+        doTask2();
+    }
+    public static void doTask1() {
         // - инициализировать число Z;
         // - создать массив случайных нецелых чисел;
         // - заменить значения массива, которые больше Z на это число;
@@ -11,23 +14,52 @@ public class Algorithm {
 
         double counterElement = 0;
         double z = 16.3421573;
-        double sum = 0;
         double[] array = new double[7];
         for (int i = 0; i < array.length; i++) {
-            double randomnumber = (double) (Math.random() * (40.0 - 1.0) + 1.0);
-            array[i] = randomnumber;
+            array[i] = (double) (Math.random() * (40.0 - 1.0) + 1.0);
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < array.length; i++) {
             if (array[i] > z) {
                 counterElement++;
             }
-                if (array[i] > z) {
-                    array[i] = z;
-                }
+            if (array[i] > z) {
+                array[i] = z;
+            }
             System.out.print(array[i] + " ");
-            System.out.println();
             if (array[i] > z) {
                 counterElement++;
             }
         }
+        System.out.println();
         System.out.println(counterElement + " ");
     }
+
+    public static void doTask2() {
+        // создать массив содержащий нецелые числа;
+        // заполнить массив случайными числами;
+        // подсчитать количество положительных, отрицатильных и нулевых элементов;
+        // вывести результат в консоль.
+
+        double[] array = new double[12];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = ((double) (Math.random() * 201) - 100);
+        }
+        double positiveCounter = 0;
+        double negativeCounter = 0;
+        double zeroCounter = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 0) {
+                positiveCounter++;
+            } else if (array[i] < 0) {
+                negativeCounter++;
+            } else if (array[i] == 0) {
+                zeroCounter++;
+            }
+        }
+        System.out.print(positiveCounter + " " + negativeCounter + " " + zeroCounter);
+        System.out.println();
+    }
 }
+
