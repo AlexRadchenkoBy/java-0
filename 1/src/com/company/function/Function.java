f5487e0f1cfca1533b001439619f8d0da992f28b
Alexander Radchenko <alex.radchenko.by@gmail.com>
2021-12-30 21:09:03 +0300

    Function 1

--- 1/src/com/company/function/Function.java
+++ 1/src/com/company/function/Function.java
@@ -3,17 +3,8 @@ package com.company.function;
 public class Function {
 
     public static void main(String[] args) {
-        Function functionClass = new Function();
-        functionClass.getArr(1, 2);
-        functionClass.getArr(4, 5);
+       Helper helper = new Helper();
+       double square = helper.getsHexagon(4);
+       System.out.println(square);
     }
-
-
-    private int[] getArr(int a, int b) {
-        return new int[] {a, b};
-    }
-
-    private void getArr2(int a, int b) {
-    }
-
 }
---
+++ 1/src/com/company/function/Helper.java
@@ -0,0 +1,7 @@
+package com.company.function;
+
+public class Helper {
+    public double getsHexagon(int a) {
+        return ((3 * Math.sqrt(3)) / 2) * (a * a);
+    }
+}
