28d68bc7fb01297d044cb0ac0239d9129d433f36
Alexander Radchenko <alex.radchenko.by@gmail.com>
2022-01-03 21:19:26 +0300

    Function 3

--- 1/src/com/company/function/Function.java
+++ 1/src/com/company/function/Function.java
@@ -4,12 +4,16 @@ public class Function {
 
     public static void main(String[] args) {
        Helper helper = new Helper();
-       double square = helper.getsHexagon(4);
+       double square = helper.getHexagonSquare(4);
        System.out.println(square);
 
        int a = (int) (Math.random() * (20 - 1) + 1);
        int b = (int) (Math.random() * (20 - 1) + 1);
        int c = (int) (Math.random() * (20 - 1) + 1);
-       boolean primes = helper.getsPrimeNumbers(a, b, c);
+       boolean coprime = helper.areСoprime(25, 5, 3);
+       System.out.println(coprime);
+
+       int Nod = helper.getNod(25, 14);
+        System.out.println(Nod);
     }
 }
--- 1/src/com/company/function/Helper.java
+++ 1/src/com/company/function/Helper.java
@@ -1,12 +1,31 @@
 package com.company.function;
 
 public class Helper {
-    public double getsHexagon(int a) {
+    public double getHexagonSquare(int a) {
         return ((3 * Math.sqrt(3)) / 2) * (a * a);
     }
 
+    public int getNod(int a, int b) {
+        int min;
+        int nod = 0;
+        if (a > b) {
+            min = b;
+        } else {
+            min = a;
+        }
+        for (int count = 1; count <= min; count++) {
+            if (a % count == 0 && b % count == 0) {
+                if (count > nod) {
+                    nod = count;
+                }
+            }
+        }
+        return nod;
+    }
 
-    public boolean getsPrimeNumbers(int a, int b, int c) {
-
+    public boolean areСoprime(int a, int b, int c) {
+        return (getNod(a, b) == 1 && getNod(a, c) == 1)
+                || (getNod(b, a) == 1 && getNod(b, c) == 1)
+                || (getNod(c, a) == 1 && getNod(c, b) == 1);
     }
 }
