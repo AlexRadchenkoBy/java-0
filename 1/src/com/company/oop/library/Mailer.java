8949d61a6fcab359def1249e166bc6b726129ec6
Alexander Radchenko <alex.radchenko.by@gmail.com>
2022-05-15 22:12:56 +0300

    Class 70

--- 1/src/com/company/oop/library/Mailer.java
+++ 1/src/com/company/oop/library/Mailer.java
@@ -1,14 +1,52 @@
 package com.company.oop.library;
 
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import java.util.Properties;
 
 public class Mailer {
-    public void send(String email, String text) {
-        String from = email;
-        String to = "test@test";
-        String host = "127.0.0.1";
+    public static void send(String email, String text) {
+        String from = "alex.radchenko.by@gmail.com";
+        String password = "ВСТАВИТЬ ПАРОЛЬ";
+        String to = "alex.radchenko.by@gmail.com";
 
+        final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
         Properties properties = System.getProperties();
-        properties.setProperty("mail.smtp.host", host);
+        properties.setProperty("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.ssl.enable", true);
+        properties.put("mail.smtp.ssl.protocols", "TLSv1.2");
+        properties.setProperty("mail.smtp.port", "465");
+        properties.setProperty("mail.smtp.socketFactory.port", "465");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.debug", "true");
+        properties.put("mail.store.protocol", "pop3");
+        properties.put("mail.transport.protocol", "smtp");
+
+        Session session = Session.getDefaultInstance(properties,
+                new Authenticator(){
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(from, password);
+                    }});
+
+
+        try {
+            MimeMessage message = new MimeMessage(session); // email message
+
+            message.setFrom(new InternetAddress(from)); // setting header fields
+
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+            message.setSubject("Test Mail from Java Program"); // subject line
+
+            // actual mail body
+            message.setText(text);
+
+            // Send message
+            Transport.send(message); System.out.println("Email Sent successfully....");
+        } catch (MessagingException mex){ mex.printStackTrace(); }
+
     }
 }
+
+
--- 1/src/com/company/oop/library/Main.java
+++ 1/src/com/company/oop/library/Main.java
@@ -2,7 +2,8 @@ package com.company.oop.library;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Library library = new Library();
-        library.runInProgram();
+       // Library library = new Library();
+       // library.runInProgram();
+        Mailer.send("anthony.by.ateo@gmail.com", "Привет!");
     }
 }
