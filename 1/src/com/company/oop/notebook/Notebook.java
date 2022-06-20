4c8feaaaff2472f2f2ac9953450ddfbdc4b207ab
Alexander Radchenko <alex.radchenko.by@gmail.com>
2022-06-20 16:38:00 +0300

    Class 95

--- 1/src/com/company/oop/notebook/Notebook.java
+++ 1/src/com/company/oop/notebook/Notebook.java
@@ -3,6 +3,9 @@ package com.company.oop.notebook;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.regex.Pattern;
 
@@ -67,15 +70,14 @@ public class Notebook {
         return record;
     }
 
-    public ArrayList<Note> searchRecord1(String date) {
-        Pattern pattern = Pattern.compile(date);
+    // public ArrayList<Note> searchRecord1(String date) {
+    //   Pattern pattern = Pattern.compile(date);
+    // }
 
-    }
-
-    public ArrayList<Note> searchRecord(String date) {
+    public ArrayList<Note> searchRecord(String date) throws ParseException {
         ArrayList<Note> record = new ArrayList<>();
         for (Note note : notes) {
-            if (Objects.equals(date, note.getCreationDate())) {
+            if (Objects.equals(new SimpleDateFormat("dd MM yyyy").parse(date), note.getCreationDate())) {
                 record.add(note);
             }
         }
@@ -90,46 +92,48 @@ public class Notebook {
         }
     }
 
-        public void addNote () throws Exception {
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Тема: ");
-            String topic = scanner.nextLine();
-            System.out.print("Дата создания: ");
-            String date = scanner.nextLine();
-            System.out.print("Email: ");
-            String email = scanner.nextLine();
-            System.out.print("Сообщение: ");
-            String message = scanner.nextLine();
-            createNote(topic, date, email, message);
-        }
-
-        public void createNote (String topic, Date creationDate, String email, String message) throws IOException {
-            Note note = new Note(topic, creationDate, email, message);
-            notes.add(note);
-            saveNote(note, true);
-        }
-
-        public void saveNote (Note note,boolean append) throws IOException {
-            FileWriter fileWriter = new FileWriter(NOTE_PATH, append);
-            fileWriter.write("\n" + note.getTopic() + "," + note.getCreationDate() + "," + note.getEmail() + "," +
-                    note.getMessage());
-            fileWriter.flush();
-        }
-
-        public ArrayList<Note> readNote (String pathToFile) throws Exception {
-            ArrayList<String> rows = new ArrayList<>();
-            FileReader file = new FileReader(pathToFile);
-            Scanner scanner = new Scanner(file);
-            while (scanner.hasNextLine()) {
-                rows.add(scanner.nextLine());
-            }
-            ArrayList<Note> notes = new ArrayList<>();
-            for (int i = 0; i < rows.size(); i++) {
-                String[] fields = rows.get(i).split(",");
-                Note note = new Note(fields[0], fields[1], fields[2], fields[3]);
-                notes.add(note);
-            }
-            return notes;
-        }
+    public void addNote() throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Тема: ");
+        String topic = scanner.nextLine();
+        System.out.print("Email: ");
+        String email = scanner.nextLine();
+        System.out.print("Сообщение: ");
+        String message = scanner.nextLine();
+        createNote(topic, email, message);
     }
+
+    public void createNote(String topic, String email, String message) throws IOException {
+        Note note = new Note(topic, email, message);
+        notes.add(note);
+        saveNote(note, true);
+    }
+
+    public void saveNote(Note note, boolean append) throws IOException {
+        FileWriter fileWriter = new FileWriter(NOTE_PATH, append);
+        Date date = Calendar.getInstance().getTime();
+        DateFormat dateFormat = new SimpleDateFormat("dd MM yyyy");
+        String strDate = dateFormat.format(date);
+        fileWriter.write("\n" + note.getTopic() + "," + strDate + "," + note.getEmail() + "," +
+                note.getMessage());
+        fileWriter.flush();
+    }
+
+    public ArrayList<Note> readNote(String pathToFile) throws Exception {
+        ArrayList<String> rows = new ArrayList<>();
+        FileReader file = new FileReader(pathToFile);
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+            rows.add(scanner.nextLine());
+        }
+        ArrayList<Note> notes = new ArrayList<>();
+        for (int i = 0; i < rows.size(); i++) {
+            String[] fields = rows.get(i).split(",");
+            Note note = new Note(fields[0], new SimpleDateFormat("dd MM yyyy").parse(fields[1]), fields[2],
+                    fields[3]);
+            notes.add(note);
+        }
+        return notes;
+    }
+}
 
